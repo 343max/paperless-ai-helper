14ef112409098444735921ca7937cab95fe36c19
Max von Webel <max@343max.de>
2024-02-25 16:31:17 +0100

    added some try/catch blocks

--- src/main.ts
+++ src/main.ts
@@ -10,23 +10,32 @@ const port = 8080
 
 app.post("/process/:documentId", async (req, res) => {
   const documentId = parseInt(z.object({ documentId: z.string() }).parse(req.params).documentId)
-  await processDocument(documentId)
+  try {
+    await processDocument(documentId)
 
-  res.send(JSON.stringify({ success: true }))
+    res.send(JSON.stringify({ success: true }))
+  } catch (error) {
+    console.error(error)
+    res.send(JSON.stringify({ success: false }))
+  }
 })
 
 const main = async () => {
-  const documentIds = await findDocumentsWithTagId(paperlessConfig.processTagId)
+  try {
+    const documentIds = await findDocumentsWithTagId(paperlessConfig.processTagId)
 
-  if (documentIds.length === 0) {
-    console.log("No documents to process. going back to sleep.")
-    return
-  }
+    if (documentIds.length === 0) {
+      console.log("No documents to process. going back to sleep.")
+      return
+    }
 
-  console.log(`Will process these documents: ${JSON.stringify(documentIds)}`)
+    console.log(`Will process these documents: ${JSON.stringify(documentIds)}`)
 
-  for (const documentId of documentIds) {
-    await processDocument(documentId)
+    for (const documentId of documentIds) {
+      await processDocument(documentId)
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
 
